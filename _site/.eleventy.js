702242858611eeaa37725b9c8d8dfd8c39801b7f
Jordan Hatch <jordan@jordanh.net>
2021-01-23 14:25:11 +1100

    Initial commit

---
+++ _site/.eleventy.js
@@ -0,0 +1,14 @@
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation')
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyNavigationPlugin)
+
+  return {
+    dir: {
+      input: '../',
+      output: 'build/',
+      layouts: '_site/layouts',
+      data: '_site/data'
+    }
+  }
+}
