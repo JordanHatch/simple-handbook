db43568547dd58187113834b0bcb06f5403568cb
Jordan Hatch <jordan@jordanh.net>
2021-01-26 16:07:46 +1100

    First pass at a responsive navigation

--- _site/assets/js/navigation.js
+++ _site/assets/js/navigation.js
@@ -1,4 +1,20 @@
 const initNavigation = function () {
+  initCollapsibleNav()
+  initParentLists()
+}
+
+const initCollapsibleNav = function () {
+  const nav = document.querySelector('nav.global')
+
+  const title = nav.querySelector('h1')
+  const toggle = createToggleEl(nav.classList)
+
+  nav.classList.add('collapsible')
+  nav.classList.add('closed')
+  title.prepend(toggle)
+}
+
+const initParentLists = function () {
   const parentLists = document.querySelectorAll('nav.global li.has-children')
 
   parentLists.forEach(function (list, index) {
@@ -7,32 +23,32 @@ const initNavigation = function () {
     classList.add('collapsible')
     classList.add(isSelected(list) ? 'open' : 'closed')
 
-    const toggle = document.createElement('i')
-    toggle.className = 'toggle'
-    toggle.addEventListener('click', () => {
-      if (classList.contains('closed')) {
-        classList.remove('closed')
-        classList.add('open')
-      } else {
-        classList.remove('open')
-        classList.add('closed')
-      }
-    })
+    const toggle = createToggleEl(classList)
 
     list.prepend(toggle)
   })
 }
 
 const isSelected = function (list) {
-  if (list.classList.contains('selected')) {
-    return true
-  }
+  return (list.classList.contains('selected') ||
+    list.querySelectorAll('li.selected').length > 0)
+}
 
-  if (list.querySelectorAll('li.selected').length > 0) {
-    return true
-  }
+const createToggleEl = function (classList) {
+  const toggle = document.createElement('i')
+  toggle.className = 'toggle'
 
-  return false
+  toggle.addEventListener('click', () => {
+    if (classList.contains('closed')) {
+      classList.remove('closed')
+      classList.add('open')
+    } else {
+      classList.remove('open')
+      classList.add('closed')
+    }
+  })
+
+  return toggle
 }
 
 document.addEventListener('DOMContentLoaded', initNavigation)
